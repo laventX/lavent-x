c00a3ebb1db89bb32b48c8b0aa109d4bda8f569c
laventX <laventmx@gmail.com>
2024-06-25 12:29:53 +0300

    Add custom color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,16 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
+  theme: {
+    extend: {
+      colors: {
+        space: {
+          '500': '#09070c'
+        }
+      }
+    }
+  },
   plugins: []
 };
+
 export default config;

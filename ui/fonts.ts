0ae68bc294173a2acda1fb67b44c98f76e05c820
laventX <laventmx@gmail.com>
2024-06-29 17:30:45 +0300

    Add font Source Code Pro

--- ui/fonts.ts
+++ ui/fonts.ts
@@ -1,3 +1,7 @@
-import { Exo_2 } from 'next/font/google';
+import { Exo_2, Source_Code_Pro } from 'next/font/google';
 
 export const exo2 = Exo_2({ subsets: ['latin', 'cyrillic'], display: 'swap' });
+export const sourceCodePro = Source_Code_Pro({
+  subsets: ['latin', 'cyrillic'],
+  display: 'swap'
+});

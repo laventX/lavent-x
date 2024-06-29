227cd3b4dba5b667e446ec3fb9de8f7ccfcb3ae1
laventX <laventmx@gmail.com>
2024-06-29 10:19:44 +0300

    Make logo responsive

--- components/logo/logo.tsx
+++ components/logo/logo.tsx
@@ -22,11 +22,17 @@ type LaventProps = CustomComponentProps;
 
 function Lavent({ className }: LaventProps) {
   return (
-    <p className={clsx(className, 'text-6xl uppercase [font-weight:725]')}>
-      <span className="-tracking-[6px]">La</span>
-      <span className="-tracking-[3px]">v</span>
-      <span className="-tracking-[1px]">e</span>
-      <span className="-tracking-[6px]">n</span>t
+    <p
+      className={clsx(
+        className,
+        'text-6xl uppercase [font-weight:725] lg:text-5xl'
+      )}
+    >
+      <span className="-tracking-[6px] md:sr-only">La</span>
+      <span className="-tracking-[3px] md:sr-only">v</span>
+      <span className="-tracking-[1px] md:sr-only">e</span>
+      <span className="-tracking-[6px] md:sr-only">n</span>
+      <span className="md:sr-only">t</span>
       <span className="sr-only">X</span>
     </p>
   );
@@ -65,15 +71,30 @@ function X({ className }: XProps) {
     <div
       className={clsx(
         className,
-        'flex h-[160px] w-[160px] cursor-pointer items-center justify-center'
+        'flex h-[160px] w-[160px] cursor-pointer items-center justify-center lg:h-[120px] lg:w-[120px] sm:h-[80px] sm:w-[80px]'
       )}
       onClick={handleOnClick}
     >
-      <div className="h-[116px] w-[118px] origin-[59px_58px]" ref={xRef}>
-        <Dash className="-translate-x-[7px] translate-y-[19px]" angle="\" />
-        <Dash className="translate-x-[56px] translate-y-[3px]" angle="/" />
-        <Dash className="-translate-x-[7px] translate-y-[49px]" angle="/" />
-        <Dash className="translate-x-[56px] translate-y-[33px]" angle="\" />
+      <div
+        className="h-[116px] w-[118px] origin-[59px_58px] lg:h-[87px] lg:w-[89px] lg:origin-[45px_44px] sm:h-[58px] sm:w-[59px] sm:origin-[29px_29px]"
+        ref={xRef}
+      >
+        <XDash
+          className="-translate-x-[7px] translate-y-[19px] lg:-translate-x-[5px] lg:translate-y-[15px] sm:-translate-x-[4px] sm:translate-y-[9px]"
+          angle="\"
+        />
+        <XDash
+          className="translate-x-[56px] translate-y-[3px] lg:translate-x-[43px] lg:translate-y-[3px] sm:translate-x-[28px] sm:translate-y-[2px]"
+          angle="/"
+        />
+        <XDash
+          className="-translate-x-[7px] translate-y-[49px] lg:-translate-x-[5px] lg:translate-y-[37px] sm:-translate-x-[4px] sm:translate-y-[25px]"
+          angle="/"
+        />
+        <XDash
+          className="translate-x-[56px] translate-y-[33px] lg:translate-x-[43px] lg:translate-y-[25px] sm:translate-x-[28px] sm:translate-y-[17px]"
+          angle="\"
+        />
       </div>
     </div>
   );
@@ -83,12 +104,12 @@ type DashProps = CustomComponentProps & {
   angle?: '\\' | '/';
 };
 
-function Dash({ className, angle = '\\' }: DashProps) {
+function XDash({ className, angle = '\\' }: DashProps) {
   return (
     <div
       className={clsx(
         className,
-        'h-[16px] w-[69px] rounded-full bg-white',
+        'h-[16px] w-[69px] rounded-full bg-white lg:h-[12px] lg:w-[52px] sm:h-[8px] sm:w-[35px]',
         angle === '\\' ? 'rotate-45' : '-rotate-45'
       )}
     ></div>

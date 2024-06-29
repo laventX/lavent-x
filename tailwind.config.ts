87fd3576194ff18a16a48987c16eaf9114337ba0
laventX <laventmx@gmail.com>
2024-06-29 10:15:41 +0300

    Switch to a desktop-first breakpoint system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,13 @@ const config: Config = {
           '500': '#09070c'
         }
       }
+    },
+    screens: {
+      '2xl': { max: '1535px' },
+      'xl': { max: '1279px' },
+      'lg': { max: '1023px' },
+      'md': { max: '767px' },
+      'sm': { max: '639px' }
     }
   },
   plugins: []
